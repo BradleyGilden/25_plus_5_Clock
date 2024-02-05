45bf694c178ac78508bc876f6cf960b88748b1eb
BradleyGilden <bradleygilden@gmail.com>
2024-02-05 10:18:04 +0200

    clean up

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   const [playing, setPlaying] = useState(false);
   const [isSession, setIsSession] = useState(true);
   const [isBeeping, setIsBeeping] = useState(false);
+  const clockRef = useRef(null);
   const props = {
     breakLength, setBreakLength,
     sessionLength, setSessionLength,
@@ -23,8 +24,8 @@ function App() {
       <div className="flex flex-col  py-10 items-center h-screen w-screen sm:w-[min(100vw,80vh)]">
         <h1 id="title" className="text-4xl mb-10 text-center">25 + 5 Clock</h1>
         <TimeControls {...props} />
-        <Clock {...props} />
-        <PlayControls {...props} />
+        <Clock {...props} clockRef={clockRef} />
+        <PlayControls {...props} clockRef={clockRef} />
       </div>
     </div>
   );
@@ -97,12 +98,13 @@ const SessionBreak = (props) => {
 
 // displays the time left in a session or break
 const Clock = (props) => {
-  const { timeLeft, sessionLength, isSession } = props;
+  const { timeLeft, sessionLength, isSession, clockRef, playing } = props;
 
-  let minutes = sessionLength < 60 ? timeLeft.getMinutes().toString().padStart(2, '0') : '60';
+  let minutes = sessionLength < 60 || (timeLeft.getMinutes() < 60 && playing) ? timeLeft.getMinutes().toString().padStart(2, '0') : '60';
   let seconds = timeLeft.getSeconds().toString().padStart(2, '0');
+  console.log(minutes, ":", seconds);
   return (
-    <div id="clock" className="w-1/2 mt-5 py-10 sm:w-[calc(min(100vw,80vh)*0.5)] h-[calc(100vw*0.5)] sm:h-[calc(min(100vw,80vh)*0.5)] text-bg bg-fg rounded-full">
+    <div ref={clockRef} id="clock" className="w-1/2 mt-5 py-10 sm:w-[calc(min(100vw,80vh)*0.5)] h-[calc(100vw*0.5)] sm:h-[calc(min(100vw,80vh)*0.5)] text-bg bg-fg rounded-full">
       <h2 id="timer-label" className="text-2xl sm:text-3xl text-center">{ isSession ? "Session": "Break" }</h2>
       <h1 id="time-left" className="text-center text-[calc(min(100vw,80vh)*0.13)] sm:mt-2">{minutes}:{seconds}</h1>
     </div>
@@ -118,6 +120,7 @@ const PlayControls = (props) => {
     breakLength, setBreakLength,
     isSession, setIsSession,
     isBeeping, setIsBeeping,
+    clockRef
   } = props;
 
   // keep track of setInterval asynchronous functionality
@@ -128,13 +131,12 @@ const PlayControls = (props) => {
 
   // toggle the playing state
   const toggle = () => {
-    // let prev = playing;
+    let prev = playing;
     setPlaying(!playing);
-    // if (!prev) {
-    //   // if the state was previously paused, immediatly decrement
-    //   let newTime = new Date(Number(timeLeft) - 1000);
-    //   setTimeLeft(newTime);
-    // }
+    if (!prev) {
+      // if the state was previously paused, immediatly decrement
+      setTimeLeft(new Date(Number(timeLeft) - 1000));
+    }
   }
   // reset the timer
   const reset = () => {
@@ -146,11 +148,18 @@ const PlayControls = (props) => {
     audioRef.current.currentTime = 0;
     setIsBeeping(false);
     setIsSession(true);
+    clockRef.current.style.backgroundColor = "#3B372E";
+  }
+
+  const handleEnd = () => {
+    setIsBeeping(false)
+    clockRef.current.style.backgroundColor = "#3B372E";
   }
 
   useEffect(() => {
     let newTime;
     const playPause = () => {
+      console.log(timeLeft)
       if (timeLeft.getSeconds() === 0 && timeLeft.getMinutes() === 0 && isSession) {
         setTimeLeft(new Date(breakLength*60*1000));
         setIsSession(false)
@@ -163,11 +172,12 @@ const PlayControls = (props) => {
         if (newTime.getSeconds() === 0 && newTime.getMinutes() === 0) {
           setIsBeeping(true);
           audioRef.current.play();
+          clockRef.current.style.backgroundColor = "red";
         }
       }
     }
     intervalRef.current = setInterval(() => {
-      if (playing) {
+      if (playing && !isBeeping) {
         playPause();
       }
     }, 1000);
@@ -182,7 +192,7 @@ const PlayControls = (props) => {
 
   return (
     <div className="flex gap-5 w-3/5 justify-center items-center mt-6">
-      <audio id="beep" ref={audioRef} src={audioFile} onEnded={() => setIsBeeping(false)} />
+      <audio id="beep" ref={audioRef} src={audioFile} onEnded={handleEnd} />
       <button id="start_stop" type="button" onClick={toggle} className="bg-play-pause bg-contain bg-no-repeat h-20 w-20"></button>
       <button id="reset" type="button" onClick={reset} className="bg-reset bg-contain bg-no-repeat h-[4.5rem] w-[4.5rem]"></button>
     </div>
